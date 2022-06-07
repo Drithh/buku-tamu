1e7b1e4c51e33ad2c3de238382a95ad91902535d
drithh <adrielalfeus@student.uns.ac.id>
2022-06-07 12:15:48 +0700

    auto create table

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'us-cdbr-east-05.cleardb.net');
 define('DB_USER', 'bc0d2641bc9fd0');
 define('DB_PASS', '2574b9df');
-define('DB_NAME', 'buku_tamu');
+define('DB_NAME', 'heroku_6af49f79215f3d1');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS);
 if ($conn->connect_error) {
@@ -14,6 +14,20 @@ if ($conn->connect_error) {
 $query = 'SHOW DATABASES LIKE "' . DB_NAME . '"';
 if ($conn->query($query)->num_rows === 1) {
     $conn->select_db(DB_NAME);
+    $sqlTable = "CREATE TABLE " . DB_NAME . ".data_tamu (
+        id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+        nama VARCHAR(50) NOT NULL,
+        email VARCHAR(50),
+        tanggal DATETIME NOT NULL,
+        komentar TEXT NOT NULL,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
+
+
+    if ($conn->query($sqlTable) === TRUE) {
+        echo "<h2>Table berhasil dibuat</h2><br>";
+        echo '<button><a style="text-decoration: none; color=black" href="index.php">Kembali Ke Halaman Utama</a></button>';
+    }
     if ($conn->connect_errno) {
         var_dump($conn);
         echo "Gagal Connect ke Database : (" . $conn->connect_errno . ") " . $conn->connect_error;
--- create-database.php
+++ create-database.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'buku_tamu');
+define('DB_HOST', 'us-cdbr-east-05.cleardb.net');
+define('DB_USER', 'bc0d2641bc9fd0');
+define('DB_PASS', '2574b9df');
+define('DB_NAME', 'heroku_6af49f79215f3d1');
 
 // Create connection
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS);
@@ -14,6 +14,9 @@ if ($conn->connect_error) {
 // Create database
 $sql = "CREATE DATABASE " . DB_NAME;
 if ($conn->query($sql) === TRUE) {
+
+    echo "<h2>Database berhasil dibuat</h2><br>";
+
     $sqlTable = "CREATE TABLE " . DB_NAME . ".data_tamu (
         id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
         nama VARCHAR(50) NOT NULL,
@@ -23,7 +26,6 @@ if ($conn->query($sql) === TRUE) {
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     )";
 
-    echo "<h2>Database berhasil dibuat</h2><br>";
 
     if ($conn->query($sqlTable) === TRUE) {
         echo "<h2>Table berhasil dibuat</h2><br>";
